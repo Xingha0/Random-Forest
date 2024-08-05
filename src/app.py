2b2b73fcc49cc9d936ad57bc789e603cc4100a12
Jorge Alvarez Garcia <114688583+Xingha0@users.noreply.github.com>
2024-08-05 11:40:58 +0200

    ok

--- src/app.py
+++ src/app.py
@@ -2,3 +2,9 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+import pandas as pd
+
+train_data = pd.read_csv("../data/processed/clean_train.csv")
+test_data = pd.read_csv("../data/processed/clean_test.csv")
+
+train_data.head()
